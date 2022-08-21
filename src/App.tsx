0a3d5245c1ea180bd12798359a0a247659e2d629
Wemerson <wemersonqueiroz@yahoo.com>
2022-08-21 08:53:42 +0100

    updating padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Navbar from "./containers/Navbar";
-import Hero from "./containers/Hero";
-import Main from "./containers/Main";
+import React from "react"
+import Navbar from "./containers/Navbar"
+import Hero from "./containers/Hero"
+import Main from "./containers/Main"
 
 function App() {
   return (
     <>
-        <div className="relative container mx-auto justify-center min-h-screen items-center bg-clrBackgroundPrimary text-clrText font-poppins ">
-      <Navbar />
-      <Hero />
-      <Main/>
-    </div>
+      <div className="relative container px-8 mx-auto justify-center min-h-screen items-center bg-clrBackgroundPrimary text-clrText font-poppins ">
+        <Navbar />
+        <Hero />
+        <Main />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
